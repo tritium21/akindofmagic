d0620b6b8c35e403237512c3f2dce26e10080cdc
alex walters <tritium@sdamon.com>
2017-05-08 06:03:30 -0400

    Tests changed to run in ptvs.

--- magic/test/test_magic.py
+++ magic/test/test_magic.py
@@ -1,26 +1,32 @@
-import magic
+import unittest
 import os.path
+
+import magic
 
 TEST_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'test.zip')
 
-def test_from_file_mime():
-    expected = 'application/zip'
-    actual = magic.from_file(TEST_FILE, mime=True)
-    assert actual == expected
+class TestMagic(unittest.TestCase):
+    def test_from_file_mime(self):
+        expected = 'application/zip'
+        actual = magic.from_file(TEST_FILE, mime=True)
+        self.assertEqual(actual, expected)
 
-def test_from_file():
-    expected = 'Zip archive data'
-    actual = magic.from_file(TEST_FILE)
-    assert actual.startswith(expected)
+    def test_from_file(self):
+        expected = 'Zip archive data'
+        actual = magic.from_file(TEST_FILE)
+        self.assertTrue(actual.startswith(expected))
 
-def test_from_buffer_mime():
-    expected = 'application/zip'
-    with open(TEST_FILE, 'br') as f:
-        actual = magic.from_buffer(f.read(), mime=True)
-    assert actual.startswith(expected)
+    def test_from_buffer_mime(self):
+        expected = 'application/zip'
+        with open(TEST_FILE, 'br') as f:
+            actual = magic.from_buffer(f.read(), mime=True)
+        self.assertEqual(actual, expected)
 
-def test_from_buffer():
-    expected = 'Zip archive data'
-    with open(TEST_FILE, 'br') as f:
-        actual = magic.from_buffer(f.read())
-    assert actual.startswith(expected)
+    def test_from_buffer(self):
+        expected = 'Zip archive data'
+        with open(TEST_FILE, 'br') as f:
+            actual = magic.from_buffer(f.read())
+        self.assertTrue(actual.startswith(expected))
+
+if __name__ == '__main__':
+   unittest.main()
