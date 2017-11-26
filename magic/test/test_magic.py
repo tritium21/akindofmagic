0a3fe50249c638f3e456e724f20bf20a26147512
alex walters <tritium@sdamon.com>
2017-11-26 01:05:32 -0500

    Added prints to tests, to see how they are failing.

--- magic/test/test_magic.py
+++ magic/test/test_magic.py
@@ -13,29 +13,34 @@ class TestMagic(unittest.TestCase):
     def test_from_file_mime(self):
         expected = 'application/zip'
         actual = magic.from_file(TEST_FILE, mime=True)
+        print(actual)
         self.assertEqual(actual, expected)
 
     def test_from_file(self):
         expected = 'Zip archive data'
         actual = magic.from_file(TEST_FILE)
+        print(actual)
         self.assertTrue(actual.startswith(expected))
 
     def test_from_buffer_mime(self):
         expected = 'application/zip'
         with io.open(TEST_FILE, 'br') as f:
             actual = magic.from_buffer(f.read(), mime=True)
+        print(actual)
         self.assertEqual(actual, expected)
 
     def test_from_buffer(self):
         expected = 'Zip archive data'
         with io.open(TEST_FILE, 'br') as f:
             actual = magic.from_buffer(f.read())
+        print(actual)
         self.assertTrue(actual.startswith(expected))
 
     def test_uncompress(self):
         m = magic.Magic(uncompress=True)
         expected = 'gzip compressed data, was "test.txt"'
         actual = m.from_file(TEST_GZIP)
+        print(actual)
         self.assertTrue(actual.startswith(expected))
 
 
